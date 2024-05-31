f46ed66f9ce41ba32b4fc9130d33655159f04d72
YazidVillasana <yazid.villasasna@gmail.com>
2024-05-31 13:04:08 -0600

    1.01 Empezando a configurar el entorno de trabajo
    1.02 Agregando clase ConvertidorDeMonedas donde se configurará la api
    
    1.03 Agregando Record para llamar las divisas y la conversión.
    
    1.04 Corrigiendo un error en el record "CompararPrecios" de coda a code
    
    1.05 Se creo un método para llamar la comparasión de divisas en el record "Comparar Precios." se mantiene en standby el record "Llamar Divisas." Se comenzó a escribir el código en principal y se probó que se puede llamar a la API y se puede llamar sin problemas.
    
    1.06 En Principal, se agregó el Menú y Scanner para el input. Iniciando un "switch" para agregar cada caso del menú.
    
    1.07 Principal: crear el primer caso, aun incompleto para determinar valores. CompararParesDeDivisas: Se iban agregar variables pero se descartó. CompararPrecios se eliminó "conversion_rate" y se acomodo base_code, target_code y conversion_result. Se creó la claso "Divisas" donde se agregó 3 variables para poder llamar después de forma independiente.
    
    1.08 Divisas: Se Agregaron getters y setters, y se creó un método para llamar objetos del Json.

--- src/com/aluraclases/DesafioConvertidorMonedas/modelos/Divisas.java
+++ src/com/aluraclases/DesafioConvertidorMonedas/modelos/Divisas.java
@@ -1,12 +1,30 @@
 package com.aluraclases.DesafioConvertidorMonedas.modelos;
 
+import com.aluraclases.DesafioConvertidorMonedas.Principal.CompararPrecios;
+
 public class Divisas {
 
     private String divisaEntrada;
     private String divisaSalida;
     private float divisaConvertida;
 
+    public Divisas(CompararPrecios miCompararPrecios){
+        this.divisaEntrada = miCompararPrecios.base_code();
+        this.divisaSalida = miCompararPrecios.target_code();
+        this.divisaConvertida = miCompararPrecios.conversion_result();
+    }
 
 
-
+    public String getDivisaEntrada() {
+        return divisaEntrada;}
+    public void setDivisaEntrada(String divisaEntrada) {
+        this.divisaEntrada = divisaEntrada;}
+    public String getDivisaSalida() {
+        return divisaSalida;}
+    public void setDivisaSalida(String divisaSalida) {
+        this.divisaSalida = divisaSalida;}
+    public float getDivisaConvertida() {
+        return divisaConvertida;}
+    public void setDivisaConvertida(float divisaConvertida) {
+        this.divisaConvertida = divisaConvertida;}
 }
