948eae1778e4240255113682ac26b4900de617f8
YazidVillasana <yazid.villasasna@gmail.com>
2024-05-31 12:31:56 -0600

    1.01 Empezando a configurar el entorno de trabajo
    1.02 Agregando clase ConvertidorDeMonedas donde se configurará la api
    
    1.03 Agregando Record para llamar las divisas y la conversión.
    
    1.04 Corrigiendo un error en el record "CompararPrecios" de coda a code
    
    1.05 Se creo un método para llamar la comparasión de divisas en el record "Comparar Precios." se mantiene en standby el record "Llamar Divisas." Se comenzó a escribir el código en principal y se probó que se puede llamar a la API y se puede llamar sin problemas.
    
    1.06 En Principal, se agregó el Menú y Scanner para el input. Iniciando un "switch" para agregar cada caso del menú.

--- src/com/aluraclases/DesafioConvertidorMonedas/Principal/Principal.java
+++ src/com/aluraclases/DesafioConvertidorMonedas/Principal/Principal.java
@@ -9,16 +9,43 @@ public class Principal {
     public static void main(String[] args) {
         Scanner lectura = new Scanner(System.in);
         CompararParesDeDivisas consulta = new CompararParesDeDivisas();
-        System.out.println("Menú");
 
-        try {
+       while (true){
 
-            CompararPrecios compara = consulta.comparaDivisa("USD", "CHF", 10);
-            System.out.println(compara);
-        }catch (RuntimeException e){
-            System.out.println(e.getMessage());
-            System.out.println("Finalizando la aplicación.");
-        }
+           System.out.println("""
+                ********************************************************
+                Bienvenido/a al Conversor de Moneda ^u^
+        
+                1) Dólar ==> Peso argentino
+                2) Peso argentino ==> Dólar
+                3) Dólar ==> Real brasileño
+                4) Real brasileño ==> Dólar
+                5) Dólar ==> Peso colombiano
+                6) Peso colombiano ==> Dólar
+                7) Salir
+                Elija una opción
+                ********************************************************
+                """);
+
+           try {
+               var opcion = lectura.nextLine();
+               switch (opcion){
+
+
+
+               }
+
+               CompararPrecios compara = consulta.comparaDivisa("USD", "CHF", 10);
+               System.out.println(compara);
+               }catch (RuntimeException e){
+                   System.out.println(e.getMessage());
+                   System.out.println("Finalizando la aplicación.");
+               }
+
+
+       }
+
+
 
 
     }
